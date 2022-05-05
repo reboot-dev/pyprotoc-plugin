ab5845d203dc30686dc805a91a121a9b7c0e890c
Bulat Nizamutdinov <Bulat.Nizamutdinov@itechart-group.com>
2022-05-05 15:33:42 +0300

    added some comments

--- .github/workflows/scripts/check-format-style.sh
+++ .github/workflows/scripts/check-format-style.sh
@@ -2,7 +2,7 @@
 
 YAPF_COMMAND=$(yapf -p --diff --recursive .)
 if [[ "${YAPF_COMMAND}" ]]; then
-  echo "Please fix Python files format."
+  echo "Please fix Python files format using a command `yapf -p -i --recursive .`"
   echo "${YAPF_COMMAND}"
   exit 1
 fi
